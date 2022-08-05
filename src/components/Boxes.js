26d44e8e86281c22883afc179141cc2cb2bdd18a
Adam gif <56506180+kycksto@users.noreply.github.com>
2022-08-05 21:10:52 +0700

    Adding Missing BoxMesh

--- src/components/Boxes.js
+++ src/components/Boxes.js
@@ -1,19 +1,14 @@
 import React, { Suspense } from "react";
 
-import Box from './Box'
 import BoxMesh from "./BoxMesh";
 
 
 const Boxes = ({onClick}) => {
 
-    const linked = (url) => {
-        window.open(url, "_blank")
-    }
-
   return (
     <Suspense>
-        {/* <BoxMesh position={[ 0, 0, 2 ]} color={"blue"} onClick={linked("https://facebook.com")}/> */}
-        {/* <BoxMesh position={[ 0, 2, 0 ]} color={"green"} onClick={linked("https://google.com")}/> */}
+        <BoxMesh position={[ 0, 0, 2 ]} color={"blue"} onClick={onClick}/>
+        <BoxMesh position={[ 0, 2, 0 ]} color={"green"} onClick={onClick}/>
         <BoxMesh position={[ 0, -2, 0 ]} color={"red"} onClick={onClick}/>
         <BoxMesh position={[ 0, 0, -2 ]} color={"blue"} onClick={onClick}/>
         <BoxMesh position={[ 2, 0, 0 ]} color={"green"} onClick={onClick}/>
